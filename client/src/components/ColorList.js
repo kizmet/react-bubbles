be005f0bd905513a1900b129be23af276e1c59dd
Kizmet <bryantpatton@gmail.com>
2019-08-23 15:59:07 -0700

    add color

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -9,42 +9,72 @@ const initialColor = {
 const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  
   const [isError, setIsError] = useState(false);
+  const [adding, setAdding] = useState(false);
 
+
+  const addState = ({
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff"
+    }
+  })
+  const [colorToAdd, setColorToAdd] = useState(addState);
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
 
+  const addColor = () => {
+    setAdding(true);
+    console.log(adding)
+    
+  };
 
   const reviseData = colorToEdit => {
-    return AxiosAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
-    .then(res => {
+    return AxiosAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
         console.log(res);
         console.log(res);
-        updateColors(colors.map(color => (color.id === colorToEdit.id ? colorToEdit : color)));
-        
-      })
-
-    
+        updateColors(
+          colors.map(color =>
+            color.id === colorToEdit.id ? colorToEdit : color
+          )
+        );
+      });
   };
 
   const saveEdit = e => {
     e.preventDefault();
-    reviseData(colorToEdit)
-    console.log(colorToEdit)
+    reviseData(colorToEdit);
+    
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
+  const saveAdd = e => {
+    e.preventDefault();
+    return AxiosAuth()
+      .post(`http://localhost:5000/api/colors/`, colorToAdd)
+      .then(res => {
+        console.log(res);
+        updateColors(res.data)
+      });
+    
+    // Make a put request to save your updated color
+    // think about where will you get the id from...
+    // where is is saved right now?
+  };  
+
   const deleteColor = color => {
-    return AxiosAuth().delete(`http://localhost:5000/api/colors/${color.id}`)
-    .then(res => {
-        console.log(res);
-        console.log(res);
+    return AxiosAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
         updateColors(colors.filter(col => col.id !== color.id));
-      })    
+      });
     // make a delete request to delete this color
   };
 
@@ -67,6 +97,10 @@ const ColorList = ({ colors, updateColors }) => {
           </li>
         ))}
       </ul>
+      <div onClick={() => addColor()}>
+        <span className="delete">Add Color</span>
+      </div>
+
       {editing && (
         <form onSubmit={saveEdit}>
           <legend>edit color</legend>
@@ -97,8 +131,38 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
+      <div className="spacer" >
+      {adding && (
+        <form onSubmit={saveAdd}>
+          <legend>add color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToAdd({ ...colorToAdd, color: e.target.value })
+              }
+              value={colorToAdd.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToAdd({
+                  ...colorToAdd,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToAdd.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">save</button>
+            <button onClick={() => setEditing(false)}>cancel</button>
+          </div>
+        </form>
+      )}
+    </div>
     </div>
   );
 };
